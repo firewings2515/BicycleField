e8dd4acd26a3e74c9335ba2a5ad6de79af3b44e7
FireramaRalphadel <b10815001@gapps.ntust.edu.tw>
2021-12-15 18:15:03 +0800

    arclength
    50 - weird road
    100 - render too slow

--- Assets/Scripts/Procedural/Info.cs
+++ Assets/Scripts/Procedural/Info.cs
@@ -6,5 +6,5 @@ static public class Info
 {
     public const int MAX_LOADED_SEGMENT = 10;
     public const int PRELOAD_SEGMENT = 5;
-    public const int CHECKPOINT_SIZE = 10;
+    public const int CHECKPOINT_SIZE = 20;
 }
