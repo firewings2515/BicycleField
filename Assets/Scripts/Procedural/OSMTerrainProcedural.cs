871e5e49acc8ae6e330c8d9b2c65a9b5473c56d8
LittleLemon666 <damoncho510@gmail.com>
2021-12-23 21:27:32 +0800

    Big one
    Big one

--- Assets/Scripts/Procedural/OSMTerrainProcedural.cs
+++ Assets/Scripts/Procedural/OSMTerrainProcedural.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class OSMTerrainProcedural : MonoBehaviour
 {
     OSMEditor osm_editor;
-    int resolution = 32; //128
+    int resolution = 128; //128
     public Material terrain_mat;
     public double[] center_pos = new double[2];
     double[] center_xyz = new double[3];
@@ -34,11 +34,11 @@ public class OSMTerrainProcedural : MonoBehaviour
             center_xyz[0] = MercatorProjection.lonToX(center_pos[0]);
             center_xyz[1] = center_ele;
             center_xyz[2] = MercatorProjection.latToY(center_pos[1]);
-            double[] lefttop_pos = new double[3] { MercatorProjection.xToLon(center_xyz[0] + cam.transform.position.x - 512),
+            double[] lefttop_pos = new double[3] { MercatorProjection.xToLon(center_xyz[0] + cam.transform.position.x - 2048),
                                                    0.0f,
-                                                   MercatorProjection.yToLat(center_xyz[2] + cam.transform.position.z - 512)}; //121.5094453f, 0.0f, 25.1079903f 
-            double rightbottom_pos_x = MercatorProjection.xToLon(MercatorProjection.lonToX(lefttop_pos[0]) + 1024); // + 16384.0
-            double rightbottom_pos_z = MercatorProjection.yToLat(MercatorProjection.latToY(lefttop_pos[2]) + 1024); // + 16384.0
+                                                   MercatorProjection.yToLat(center_xyz[2] + cam.transform.position.z - 2048)}; //121.5094453f, 0.0f, 25.1079903f 
+            double rightbottom_pos_x = MercatorProjection.xToLon(MercatorProjection.lonToX(lefttop_pos[0]) + 4096); // + 16384.0
+            double rightbottom_pos_z = MercatorProjection.yToLat(MercatorProjection.latToY(lefttop_pos[2]) + 4096); // + 16384.0
             double[] terrain_size = new double[2] { rightbottom_pos_x - lefttop_pos[0], rightbottom_pos_z - lefttop_pos[2] };
             double dx = terrain_size[0] / resolution;
             double dz = terrain_size[1] / resolution;
@@ -135,102 +135,102 @@ public class OSMTerrainProcedural : MonoBehaviour
         }
         else if (is_initial)
         {
-            double[,,] terrain_points = new double[resolution + 1, resolution + 1, 3];
-            center_xyz[0] = MercatorProjection.lonToX(center_pos[0]);
-            center_xyz[1] = center_ele;
-            center_xyz[2] = MercatorProjection.latToY(center_pos[1]);
-            double[] lefttop_pos = new double[3] { MercatorProjection.xToLon(center_xyz[0] + cam.transform.position.x - 512),
-                                                   0.0f,
-                                                   MercatorProjection.yToLat(center_xyz[2] + cam.transform.position.z - 512)}; //121.5094453f, 0.0f, 25.1079903f 
-            double rightbottom_pos_x = MercatorProjection.xToLon(MercatorProjection.lonToX(lefttop_pos[0]) + 1024); // + 16384.0
-            double rightbottom_pos_z = MercatorProjection.yToLat(MercatorProjection.latToY(lefttop_pos[2]) + 1024); // + 16384.0
-            double[] terrain_size = new double[2] { rightbottom_pos_x - lefttop_pos[0], rightbottom_pos_z - lefttop_pos[2] };
-            double dx = terrain_size[0] / resolution;
-            double dz = terrain_size[1] / resolution;
+            //double[,,] terrain_points = new double[resolution + 1, resolution + 1, 3];
+            //center_xyz[0] = MercatorProjection.lonToX(center_pos[0]);
+            //center_xyz[1] = center_ele;
+            //center_xyz[2] = MercatorProjection.latToY(center_pos[1]);
+            //double[] lefttop_pos = new double[3] { MercatorProjection.xToLon(center_xyz[0] + cam.transform.position.x - 512),
+            //                                       0.0f,
+            //                                       MercatorProjection.yToLat(center_xyz[2] + cam.transform.position.z - 512)}; //121.5094453f, 0.0f, 25.1079903f 
+            //double rightbottom_pos_x = MercatorProjection.xToLon(MercatorProjection.lonToX(lefttop_pos[0]) + 1024); // + 16384.0
+            //double rightbottom_pos_z = MercatorProjection.yToLat(MercatorProjection.latToY(lefttop_pos[2]) + 1024); // + 16384.0
+            //double[] terrain_size = new double[2] { rightbottom_pos_x - lefttop_pos[0], rightbottom_pos_z - lefttop_pos[2] };
+            //double dx = terrain_size[0] / resolution;
+            //double dz = terrain_size[1] / resolution;
 
-            current_pos[0] = MercatorProjection.xToLon(center_xyz[0] + cam.transform.position.x);
-            current_pos[1] = MercatorProjection.yToLat(center_xyz[2] + cam.transform.position.z);
-            if ((int)(current_pos[0] / dx) == current_loc[0] && (int)(current_pos[1] / dz) == current_loc[1])
-            {
-                return;
-            }
-            current_loc[0] = (int)(current_pos[0] / dx);
-            current_loc[1] = (int)(current_pos[1] / dz);
+            //current_pos[0] = MercatorProjection.xToLon(center_xyz[0] + cam.transform.position.x);
+            //current_pos[1] = MercatorProjection.yToLat(center_xyz[2] + cam.transform.position.z);
+            //if ((int)(current_pos[0] / dx) == current_loc[0] && (int)(current_pos[1] / dz) == current_loc[1])
+            //{
+            //    return;
+            //}
+            //current_loc[0] = (int)(current_pos[0] / dx);
+            //current_loc[1] = (int)(current_pos[1] / dz);
 
-            //////////////////////////////get elevations/////////////////////////////////////////
-            List<float> all_elevations = new List<float>();
-            List<EarthCoord> all_coords = new List<EarthCoord>();
-            for (int i = 0; i < resolution + 1; i++)
-            {
-                for (int j = 0; j < resolution + 1; j++)
-                {
+            ////////////////////////////////get elevations/////////////////////////////////////////
+            //List<float> all_elevations = new List<float>();
+            //List<EarthCoord> all_coords = new List<EarthCoord>();
+            //for (int i = 0; i < resolution + 1; i++)
+            //{
+            //    for (int j = 0; j < resolution + 1; j++)
+            //    {
 
-                    terrain_points[i, j, 0] = lefttop_pos[0] + i * dx;
-                    terrain_points[i, j, 1] = 0.0;
-                    terrain_points[i, j, 2] = lefttop_pos[2] + j * dz;
-                    all_coords.Add(new EarthCoord((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 2]));
-                }
-            }
-            all_elevations = HgtReader.getElevations(all_coords);
-            /////////////////////////////////////////////////////////////////////////////////////
+            //        terrain_points[i, j, 0] = lefttop_pos[0] + i * dx;
+            //        terrain_points[i, j, 1] = 0.0;
+            //        terrain_points[i, j, 2] = lefttop_pos[2] + j * dz;
+            //        all_coords.Add(new EarthCoord((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 2]));
+            //    }
+            //}
+            //all_elevations = HgtReader.getElevations(all_coords);
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-            // set elevations
-            mesh = new Mesh();
-            Vector3[] vertice = new Vector3[(resolution + 1) * (resolution + 1)];
-            int[] indices = new int[6 * resolution * resolution];
-            int indices_index = 0;
-            for (int i = 0; i < resolution + 1; i++)
-            {
-                for (int j = 0; j < resolution + 1; j++)
-                {
-                    //float pos_x, pos_z;
-                    //osm_editor.osm_reader.toUnityLocation(terrain_points[i, j].x, terrain_points[i, j].z, out pos_x, out pos_z);
-                    terrain_points[i, j, 0] = MercatorProjection.lonToX(terrain_points[i, j, 0]) - center_xyz[0];
-                    double history_ele;
-                    if (searchClosing((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 2], out history_ele))
-                        terrain_points[i, j, 1] = history_ele;
-                    else
-                    {
-                        terrain_points[i, j, 1] = all_elevations[i * (resolution + 1) + j] - center_xyz[1];
-                        dp_ele.Add(new Vector2((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 2]), terrain_points[i, j, 1]);
-                    }
-                    terrain_points[i, j, 2] = MercatorProjection.latToY(terrain_points[i, j, 2]) - center_xyz[2];
-                    //terrain_points[i, j].x = pos_x;
-                    //terrain_points[i, j].y = all_elevations[i * (resolution + 1) + j];
-                    //terrain_points[i, j].z = pos_z;
-                    vertice[i * (resolution + 1) + j] = new Vector3((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 1], (float)terrain_points[i, j, 2]);
-                    //vertice2D[i * (resolution + 1) + j] = new Vector2(terrain_points[i, j].x, terrain_points[i, j].z);
-                }
-            }
+            //// set elevations
+            //mesh = new Mesh();
+            //Vector3[] vertice = new Vector3[(resolution + 1) * (resolution + 1)];
+            //int[] indices = new int[6 * resolution * resolution];
+            //int indices_index = 0;
+            //for (int i = 0; i < resolution + 1; i++)
+            //{
+            //    for (int j = 0; j < resolution + 1; j++)
+            //    {
+            //        //float pos_x, pos_z;
+            //        //osm_editor.osm_reader.toUnityLocation(terrain_points[i, j].x, terrain_points[i, j].z, out pos_x, out pos_z);
+            //        terrain_points[i, j, 0] = MercatorProjection.lonToX(terrain_points[i, j, 0]) - center_xyz[0];
+            //        double history_ele;
+            //        if (searchClosing((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 2], out history_ele))
+            //            terrain_points[i, j, 1] = history_ele;
+            //        else
+            //        {
+            //            terrain_points[i, j, 1] = all_elevations[i * (resolution + 1) + j] - center_xyz[1];
+            //            dp_ele.Add(new Vector2((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 2]), terrain_points[i, j, 1]);
+            //        }
+            //        terrain_points[i, j, 2] = MercatorProjection.latToY(terrain_points[i, j, 2]) - center_xyz[2];
+            //        //terrain_points[i, j].x = pos_x;
+            //        //terrain_points[i, j].y = all_elevations[i * (resolution + 1) + j];
+            //        //terrain_points[i, j].z = pos_z;
+            //        vertice[i * (resolution + 1) + j] = new Vector3((float)terrain_points[i, j, 0], (float)terrain_points[i, j, 1], (float)terrain_points[i, j, 2]);
+            //        //vertice2D[i * (resolution + 1) + j] = new Vector2(terrain_points[i, j].x, terrain_points[i, j].z);
+            //    }
+            //}
 
-            for (int i = 0; i < resolution; i++)
-            {
-                for (int j = 0; j < resolution; j++)
-                {
-                    // counter-clockwise
-                    indices[indices_index++] = i * (resolution + 1) + j;
-                    indices[indices_index++] = (i + 1) * (resolution + 1) + j + 1;
-                    indices[indices_index++] = (i + 1) * (resolution + 1) + j;
+            //for (int i = 0; i < resolution; i++)
+            //{
+            //    for (int j = 0; j < resolution; j++)
+            //    {
+            //        // counter-clockwise
+            //        indices[indices_index++] = i * (resolution + 1) + j;
+            //        indices[indices_index++] = (i + 1) * (resolution + 1) + j + 1;
+            //        indices[indices_index++] = (i + 1) * (resolution + 1) + j;
 
-                    indices[indices_index++] = i * (resolution + 1) + j;
-                    indices[indices_index++] = i * (resolution + 1) + j + 1;
-                    indices[indices_index++] = (i + 1) * (resolution + 1) + j + 1;
-                }
-            }
+            //        indices[indices_index++] = i * (resolution + 1) + j;
+            //        indices[indices_index++] = i * (resolution + 1) + j + 1;
+            //        indices[indices_index++] = (i + 1) * (resolution + 1) + j + 1;
+            //    }
+            //}
 
-            //Assign data to mesh
-            mesh.vertices = vertice;
-            mesh.triangles = indices;
+            ////Assign data to mesh
+            //mesh.vertices = vertice;
+            //mesh.triangles = indices;
 
-            //Recalculations
-            mesh.RecalculateNormals();
-            mesh.RecalculateBounds();
-            mesh.Optimize();
+            ////Recalculations
+            //mesh.RecalculateNormals();
+            //mesh.RecalculateBounds();
+            //mesh.Optimize();
 
-            //Name the mesh
-            mesh.name = "terrain_mesh";
+            ////Name the mesh
+            //mesh.name = "terrain_mesh";
 
-            terrain.GetComponent<MeshFilter>().mesh = mesh;
+            //terrain.GetComponent<MeshFilter>().mesh = mesh;
         }
 
         bool searchClosing(float x, float z, out double y)
