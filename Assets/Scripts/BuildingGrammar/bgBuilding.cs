1a47857d883c1d021650cbdb9a6bfe6dfabecb83
frakw <frakwu@gmail.com>
2022-04-28 13:49:24 +0800

    騎樓建築

--- Assets/Scripts/BuildingGrammar/bgBuilding.cs
+++ Assets/Scripts/BuildingGrammar/bgBuilding.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class bgBuilding : bgComponent
 {
     public List<bgBase> bases;
-    public float height = 0.0f;
+    //public float height = 0.0f;
+    public Vector3 center = new Vector3(0,0,0);
     public bgBuilding(List<string> _input_parameter, List<string> _component_parameter, List<string> _commands, List<List<string>> _commands_parameter) : base(_input_parameter, _component_parameter, _commands, _commands_parameter)
     {
 
     }
     public override GameObject build()
     {
-        height = 0.0f;
+        //height = 0.0f;
         random_background = Random.Range(0, 15);
         go = new GameObject("Building:" + name);
         for (int i = 0; i < commands.Count; i++) {
-            bgBase _base = builder.get_base(commands[i]);
-            _base.random_background = random_background;
-            GameObject obj = _base.build();      
-            obj.transform.parent = go.transform;
-            obj.transform.localPosition = new Vector3(0,height,0);
-            height += _base.height;
+            if (commands[i] == "center")
+            {
+                center = new Vector3(float.Parse(commands_parameter[i][0]), float.Parse(commands_parameter[i][1]), float.Parse(commands_parameter[i][2]));
+
+                continue;
+            }
+            else
+            {
+                bgBase _base = builder.get_base(commands[i]);
+                _base.random_background = random_background;
+                GameObject obj = _base.build();
+                obj.transform.parent = go.transform;
+                obj.transform.localPosition = center;
+                center.y += _base.height;
+            }
         }
         return go;
     }
