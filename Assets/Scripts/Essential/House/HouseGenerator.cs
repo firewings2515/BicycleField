c65a8c916dc7717cd676627cb2fd69531557f6ca
frakw <frakwu@gmail.com>
2022-07-20 19:59:56 +0800

    註解

--- Assets/Scripts/Essential/House/HouseGenerator.cs
+++ Assets/Scripts/Essential/House/HouseGenerator.cs
@@ -32,74 +32,77 @@ static public class HouseGenerator
         //gobj_db = new Dictionary<int, Dictionary<int, GameObject>>();
         //segment_id_q = new List<int>();
     }
-    static public void generateHouses(List<int> segment_id,List<int> house_id,List<string> info) {
-        queue_segment_id.Enqueue(segment_id);
-        queue_house_id.Enqueue(house_id);
-        queue_info.Enqueue(info);
+    //static public void generateHouses(List<int> segment_id, List<int> house_id, List<string> info)
+    //{
+    //    queue_segment_id.Enqueue(segment_id);
+    //    queue_house_id.Enqueue(house_id);
+    //    queue_info.Enqueue(info);
 
-    }
+    //}
 
-    static public IEnumerator generateHouse(List<int> segment_id, List<int> house_id, List<string> info)
-    {
-        int count = segment_id.Count;
-        for (int i = 0; i < count; i++)
-        {
-            generateHouse(segment_id[i], house_id[i], info[i]);
-            //break;
-            yield return new WaitForSeconds(0.3f);
-        }
-        yield return null;
-    }
+    //static public IEnumerator generateHouse(List<int> segment_id, List<int> house_id, List<string> info)
+    //{
+    //    int count = segment_id.Count;
+    //    for (int i = 0; i < count; i++)
+    //    {
+    //        generateHouse(segment_id[i], house_id[i], info[i]);
+    //        //break;
+    //        yield return new WaitForSeconds(0.3f);
+    //    }
+    //    yield return null;
+    //}
 
-    static public void generateHouse(int segment_id, int house_id, string info)
-    {
-        //demo code
-        string[] house_infos = info.Split(' ');
-        float polygon_count = float.Parse(house_infos[1]);
-        List<Vector3> points = new List<Vector3>();
-        int coord_index = 2;
-        float min_y = float.MaxValue;
-        Vector3 single_point = new Vector3(float.Parse(house_infos[coord_index]), float.Parse(house_infos[coord_index + 1]), float.Parse(house_infos[coord_index + 2]));
-        Vector3 total = new Vector3();
-        //if (TerrainGenerator.is_initial)
-        //{
-        //single_point.y = TerrainGenerator.getHeightWithBais(single_point.x, single_point.z);
-        //}
-        for (int i = 0; i < polygon_count; i++) {
-            Vector3 point = new Vector3(float.Parse(house_infos[coord_index]), float.Parse(house_infos[coord_index + 1]), float.Parse(house_infos[coord_index + 2]));
-            //if (TerrainGenerator.is_initial)
-            //{
-            //point.y = TerrainGenerator.getHeightWithBais(point.x, point.z);
-            //}
-            if (point.y < min_y) min_y = point.y;
-            points.Add(point);
-            coord_index += 3;
-            total.x += point.x;
-            total.y += point.y;
-            total.z += point.z;
-        }
-        Vector3 averge = total / polygon_count;
-        averge.y = min_y;
-        for (int i = 0; i < polygon_count; i++)
-        {
-            points[i] = new Vector3(points[i].x - averge.x, 0, points[i].z - averge.z);
-        }
+    //static public void generateHouse(int segment_id, int house_id, string info)
+    //{
+    //    //demo code
+    //    string[] house_infos = info.Split(' ');
+    //    float polygon_count = float.Parse(house_infos[1]);
+    //    List<Vector3> points = new List<Vector3>();
+    //    int coord_index = 2;
+    //    float min_y = float.MaxValue;
+    //    Vector3 single_point = new Vector3(float.Parse(house_infos[coord_index]), float.Parse(house_infos[coord_index + 1]), float.Parse(house_infos[coord_index + 2]));
+    //    Vector3 total = new Vector3();
+    //    //if (TerrainGenerator.is_initial)
+    //    //{
+    //    //single_point.y = TerrainGenerator.getHeightWithBais(single_point.x, single_point.z);
+    //    //}
+    //    for (int i = 0; i < polygon_count; i++)
+    //    {
+    //        Vector3 point = new Vector3(float.Parse(house_infos[coord_index]), float.Parse(house_infos[coord_index + 1]), float.Parse(house_infos[coord_index + 2]));
+    //        //if (TerrainGenerator.is_initial)
+    //        //{
+    //        //point.y = TerrainGenerator.getHeightWithBais(point.x, point.z);
+    //        //}
+    //        if (point.y < min_y) min_y = point.y;
+    //        points.Add(point);
+    //        coord_index += 3;
+    //        total.x += point.x;
+    //        total.y += point.y;
+    //        total.z += point.z;
+    //    }
+    //    Vector3 averge = total / polygon_count;
+    //    averge.y = min_y;
+    //    for (int i = 0; i < polygon_count; i++)
+    //    {
+    //        points[i] = new Vector3(points[i].x - averge.x, 0, points[i].z - averge.z);
+    //    }
 
-        GameObject gobj;
-        //gobj = builder.build(component_names[Random.Range(0, component_names.Length)]);
-        //gobj.transform.position = single_point;
-        gobj = build_polygon_house(points,4);
-        gobj.transform.position = averge;
-        //gobj.transform.Translate(0, single_point.y,0);
+    //    GameObject gobj;
+    //    //gobj = builder.build(component_names[Random.Range(0, component_names.Length)]);
+    //    //gobj.transform.position = single_point;
+    //    gobj = build_polygon_house(points, 4);
+    //    gobj.transform.position = averge;
+    //    //gobj.transform.Translate(0, single_point.y,0);
 
-        //gobj.transform.rotation = Quaternion.Euler(0,Random.Range(0,360),0);
-        if (!gobj_db.ContainsKey(segment_id)) {
-            gobj_db.Add(segment_id, new Dictionary<int, GameObject>());
-            segment_id_q.Add(segment_id);
-        }
-        gobj_db[segment_id].Add(house_id, gobj);
-        gobj.transform.parent = building_manager.transform;
-    }
+    //    //gobj.transform.rotation = Quaternion.Euler(0,Random.Range(0,360),0);
+    //    if (!gobj_db.ContainsKey(segment_id))
+    //    {
+    //        gobj_db.Add(segment_id, new Dictionary<int, GameObject>());
+    //        segment_id_q.Add(segment_id);
+    //    }
+    //    gobj_db[segment_id].Add(house_id, gobj);
+    //    gobj.transform.parent = building_manager.transform;
+    //}
 
     static public void destroyEarliestSegment()
     {
--- Assets/Scripts/Essential/House/HouseManager.cs
+++ Assets/Scripts/Essential/House/HouseManager.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class HouseManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        HouseGenerator.building_manager = new GameObject("BuildingManager");
-        HouseGenerator.init();
-    }
+    //void Start()
+    //{
+    //    HouseGenerator.building_manager = new GameObject("BuildingManager");
+    //    HouseGenerator.init();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (TerrainGenerator.is_initial && HouseGenerator.queue_segment_id.Count > 0)
-        {
-            StartCoroutine(HouseGenerator.generateHouse(HouseGenerator.queue_segment_id.Dequeue(), HouseGenerator.queue_house_id.Dequeue(), HouseGenerator.queue_info.Dequeue()));
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (TerrainGenerator.is_initial && HouseGenerator.queue_segment_id.Count > 0)
+    //    {
+    //        StartCoroutine(HouseGenerator.generateHouse(HouseGenerator.queue_segment_id.Dequeue(), HouseGenerator.queue_house_id.Dequeue(), HouseGenerator.queue_info.Dequeue()));
+    //    }
+    //}
 }
