deed49c92ff495a8703d56d5c8b03adbfcc20056
frakw <frakwu@gmail.com>
2022-07-21 13:41:27 +0800

    .

--- Assets/Scripts/BuildingGrammar/bgCreator/PolygonMeshInspector.cs
+++ Assets/Scripts/BuildingGrammar/bgCreator/PolygonMeshInspector.cs
@@ -9,25 +9,27 @@ public class PolygonMeshInspector : Editor
 {
     public override void OnInspectorGUI()
     {
-        //base.OnInspectorGUI();
-        //PolygonMesh2D polygon = (PolygonMesh2D) target;
-        //if (GUILayout.Button("reset"))
-        //{
-        //    //DestroyImmediate(polygon.gameObject);
-        //    //GameObject obj = new GameObject();
-        //    //polygon = obj.AddComponent<PolygonMesh2D>();
-        //}
+        base.OnInspectorGUI();
+        PolygonMesh2D polygon = (PolygonMesh2D)target;
+        if (GUILayout.Button("reset"))
+        {
+            //DestroyImmediate(polygon.gameObject);
+            //GameObject obj = new GameObject();
+            //polygon = obj.AddComponent<PolygonMesh2D>();
+        }
 
-        //if (GUILayout.Button("get polygon")) {
-        //    Vector2[] points = polygon.get_points();
-        //    List<Vector3> point3s = new List<Vector3>();
-        //    for (int i = 0; i < points.Length; i++) {
-        //        Debug.Log(points[i]);
-        //        point3s.Add(new Vector3(points[i].x, 0, points[i].y));
-        //    }
+        if (GUILayout.Button("get polygon"))
+        {
+            Vector2[] points = polygon.get_points();
+            List<Vector3> point3s = new List<Vector3>();
+            for (int i = 0; i < points.Length; i++)
+            {
+                Debug.Log(points[i]);
+                point3s.Add(new Vector3(points[i].x, 0, points[i].y));
+            }
 
 
-        //    HouseGenerator.build_polygon_house(point3s, 4);
-        //}
+            HouseGenerator.build_polygon_house(point3s, 4);
+        }
     }
 }
